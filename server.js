1b66eb4ba47c08ed277036645ee116934f3c7dd9
shivam13rb <shivam13rb@gmail.com>
2018-07-27 12:16:58 +0530

    first

---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+var app = express();
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','jade');
+
+app.use(bodyParser.json())
+//console.log(app.use(bodyParser.json()));
+//console.log(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')) )
+
+app.get('/',function(req,res){
+    
+    res.render('index',{title:'Welcome'});
+});
+
+app.get('/about',function(req,res){
+    res.render('about');
+})
+
+app.get('/contact',function(req,res){
+    res.render('contact');
+})
+
+app.post('/contact/send',function(req,res){
+    var transporter = nodemailer.createTransport({
+        service:'Gmail',
+        auth:{
+            user:'shivam13rb@gmail.com',//-------
+            pass:'9457434254@rss'//---------
+        }
+    });
+    var mailOption = {
+        from:'Shivam Shakya <shivam13rb@gmail.com>',
+        to:'shivamshakyapsit@gmail.com',
+        subject:'Website submission',
+        text:'You have a submision with the following details..... Name: '+req.body.name+'Email: '+req.body.email+'Message: '+req.body.message,
+        html:'<p>You have a submision with the following details.....</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+        
+    };
+
+    transporter.sendMail(mailOption,function(err,info){
+        if(err){
+            console.log(err);
+            res.redirect('/');
+        }
+        else{
+            console.log('Message Sent: '+info.response);
+            res.redirect('/');
+        }
+    })
+
+})
+
+app.listen(3000);
+console.log('server is running on 3000');
